0c6ee4dcfe373fbc0a929a87a197a34037454b14
오진서 <s22030@gsm.hs.kr>
2023-04-10 21:09:19 +0900

    included Lombok

--- src/main/java/com/spb/jpa/entity/ProductEntity.java
+++ src/main/java/com/spb/jpa/entity/ProductEntity.java
@@ -1,11 +1,18 @@
 package com.spb.jpa.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "product")
 public class ProductEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,56 +29,5 @@ public class ProductEntity {
     private LocalDateTime createdAt;
 
     private LocalDateTime updatedAt;
-
-
-    public Long getNumber() {
-        return number;
-    }
-
-    public void setNumber(Long number) {
-        this.number = number;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-
-    public Integer getStock() {
-        return stock;
-    }
-
-    public void setStock(Integer stock) {
-        this.stock = stock;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
 
 }
